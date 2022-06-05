755fa7b7bb198928bd8a7c3bf7a132c97ac52f03
Mutasem Alsallout <m.sallout@hotmail.com>
2022-06-05 13:30:25 +0300

    imoprting files

---
+++ Controllers/ShowListController.cs
@@ -0,0 +1,78 @@
+﻿using HomeWork1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeWork1.Controllers
+{
+    public class ShowListController : Controller
+    {
+        static List<ToDoList> mylist = new();
+        public IActionResult Index()
+        {
+            return View(nameof(Index),mylist);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create([Bind] ToDoList clist)
+        {
+            clist.id = ToDoList.currentid++;
+            clist.InsertDate = DateTime.Now;
+            mylist.Add(clist);
+            return Index();
+        }
+
+
+  
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var elist = mylist.FirstOrDefault(x => x.id == id);
+            if (elist == null)
+                return NotFound();
+            return View(elist);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, [Bind] ToDoList clist)
+        {
+            var elist = mylist.FirstOrDefault(x => x.id == id);
+            if (elist == null)
+                return NotFound();
+            elist.WhatToDo = clist.WhatToDo;
+            elist.Notes = clist.Notes;
+            elist.WhenToDo = clist.WhenToDo;
+            return Index();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var dlist = mylist.FirstOrDefault(x => x.id == id);
+            if (dlist == null)
+                return NotFound();
+           
+            return View(dlist);
+        }
+
+        [HttpPost("[Controller]/Delete/{id}")]
+        public IActionResult PostDelete(int id)
+        {
+            var dlist = mylist.FirstOrDefault(x => x.id == id);
+            if (dlist == null)
+                return NotFound();
+            mylist.Remove(dlist);
+            return Index();
+        }
+
+    }
+}
---
+++ Models/ToDoList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeWork1.Models
+{
+    public class ToDoList
+    {
+        public static int currentid { get; set; } = 1;
+        public int id { get; set; }
+        public string WhatToDo { get; set; }
+        public string Notes { get; set; }
+        public DateTime WhenToDo { get; set; }
+        public DateTime InsertDate { get; set; }
+    }
+}
